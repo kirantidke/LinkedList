a271190aa085565829c23698a475322fd1326e62
kirantidke <kirantidke2013@gmail.com>
2021-06-25 00:45:27 +0530

    "[kiran] added file"

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -4,18 +4,19 @@ public class LinkedListMain {
 
 	public static void main(String[] args) {
 		LinkedListOperations operations = new LinkedListOperations();
-		INode head;
+		
 		Node<Integer> firstNode = new Node<>(56);
 	    Node<Integer> secondNode = new Node<>(30);
 	    Node<Integer> thirdNode = new Node<>(70);
 
-	    head = operations.addNode(firstNode);
-	    operations.displayNode(head);
+	    operations.addNode(firstNode);
+	    //operations.displayNode(head);
 	    
-	    head = operations.addNode(secondNode);
-	    operations.displayNode(head);
+	    operations.addNode(secondNode);
+	    //operations.displayNode(head);
 	  
-	    head = operations.addNode(thirdNode);
-	    operations.displayNode(head);
+	    operations.addNode(thirdNode);
+	    
+	    operations.displayNode();
 	  }
 }
--- src/com/bridgelabz/linkedlist/LinkedListOperations.java
+++ src/com/bridgelabz/linkedlist/LinkedListOperations.java
@@ -1,20 +1,34 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedListOperations {
-	private INode head;
+	// Represent the head and tail of the linked list
+	public Node head = null;
+	public Node tail = null;
 
-    public INode addNode(INode node) {
-        if (head == null)
-            head = node;
-        else {
-            INode temp = head;
-            head = node;
-            head.setNext(temp);
-        }
-        return head;
-    }
-    public void displayNode(INode head) {
-        System.out.println(head.getKey());
-    }
+	public Node addNode(Node newNode) {
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		return head;
+	}
 
+	public void displayNode() {
+		INode current = head;
+		if (head == null)
+			System.out.println("Linked List is empty");
+		System.out.print("Linked nodes: ");
+		while (current != null) {
+			if (current.getNext() != null) {
+				System.out.print(current.getKey() + " -> ");
+				current = current.getNext();
+			} else {
+				System.out.println(current.getKey());
+				current = current.getNext();
+			}
+		}
+	}
 }
