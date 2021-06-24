2b4d9c12014d4f7a64050be3179c98e8bffc2be6
kirantidke <kirantidke2013@gmail.com>
2021-06-25 00:05:15 +0530

    "[kiran] added file"

---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		Node<Integer> firstNode = new Node<>(56);
+	    Node<Integer> secondNode = new Node<>(30);
+	    Node<Integer> thirdNode = new Node<>(70);
+
+	        //Linking nodes
+	     firstNode.setNext(secondNode);
+	     secondNode.setNext(thirdNode);
+
+	        //If linking successful, return successful
+	     if(firstNode.getNext() == secondNode && secondNode.getNext() == thirdNode) {
+	    	 System.out.println(firstNode.getKey() + " -> " + secondNode.getKey() + " -> " + thirdNode.getKey());
+	         System.out.println("Linked List created successfully...");
+	        }
+	        else
+	            System.out.println("Linked List creation failed...");
+	}
+
+
+}
