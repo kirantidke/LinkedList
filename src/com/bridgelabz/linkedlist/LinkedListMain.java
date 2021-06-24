e4b96d86b5a7fd5a3251a42c4cb17bc02a51bbac
kirantidke <kirantidke2013@gmail.com>
2021-06-25 02:12:04 +0530

    "[kiran] added file"

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -1,26 +1,74 @@
 /**
-* Ability to delete the first element in the LinkedList of sequence 56->30->70
+* Ability to delete the last element in the LinkedList of sequence 56->30->70
 * Write pop method. Note there is new head
-* Final Sequence: 30->70
+* Final Sequence: 56->30
 *
 */
 package com.bridgelabz.linkedlist;
 
-public class LinkedListMain {
+import java.util.Scanner;
 
+public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedListOperations operations = new LinkedListOperations();
 
 		Node<Integer> firstNode = new Node<>(56);
 		Node<Integer> secondNode = new Node<>(30);
 		Node<Integer> thirdNode = new Node<>(70);
+		boolean flag = true;
+		while (flag) {
+			int option = inputMenu();
+			switch (option) {
+			case 1:
+				operations.addNodeInBeginnig(firstNode);
+				flag = true;
+				break;
+			case 2:
+				operations.addNodeAtEnd(thirdNode);
+				flag = true;
+				break;
+			case 3:
+				operations.addNodeInMiddle(secondNode);
+				flag = true;
+				break;
+			case 4:
+				operations.deleteNodeFromBeginning();
+				flag = true;
+				break;
+			case 5:
+				operations.deleteNodeFromLast();
+				flag = true;
+				break;
+			case 6:
+				operations.displayNode();
+				flag = true;
+				break;
+			case 7:
+				operations.searchElement();
+				flag = true;
+				break;
+			case 8:
+				flag = false;
+				break;
+			}
+		}
+	}
 
-		operations.addNodeInBeginnig(firstNode);
-		operations.addNodeAtEnd(thirdNode);
-		operations.addNodeInMiddle(secondNode);
-		operations.displayNode();
+	public static int inputMenu() {
+		Scanner input = new Scanner(System.in);
 
-		operations.deleteNodeFromBeginning();
-		operations.displayNode();
+		System.out.println("Enter options to perform operations in Linked List");
+		System.out.println("Press 1 to Add Node in the Beginning");
+		System.out.println("Press 2 to Add Node at the End");
+		System.out.println("Press 3 to Add Node in the Middle");
+		System.out.println("Press 4 to Delete Node from the Beginning");
+		System.out.println("Press 5 to Delete Node from the End");
+		System.out.println("Press 6 to Display the Linked List");
+		System.out.println("Press 7 to Search Node in the Linked List");
+		System.out.println("Press 8 to Quit");
+
+		int value = input.nextInt();
+
+		return value;
 	}
 }
--- src/com/bridgelabz/linkedlist/LinkedListOperations.java
+++ src/com/bridgelabz/linkedlist/LinkedListOperations.java
@@ -2,74 +2,107 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedListOperations {
 
-	// Represent the head and tail of the singly linked list
-	INode head;
-	INode tail;
-	int size;
+	//Represent the head and tail of the singly linked list
+    INode head;
+    INode tail;
+    int size;
 
-	public void addNodeInBeginnig(Node newNode) {
-		if (head == null) {
-			head = newNode;
-			tail = newNode;
-		} else {
-			INode temp = head;
-			head = newNode;
-			head.setNext(temp);
-		}
-	}
+    public void addNodeInBeginnig(Node newNode) {
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            INode temp = head;
+            head = newNode;
+            head.setNext(temp);
+        }
+    }
 
-	public void addNodeAtEnd(Node newNode) {
-		if (head == null) {
-			head = newNode;
-			tail = newNode;
-		} else {
-			tail.setNext(newNode);
-			tail = newNode;
-		}
-		size++;
-	}
+    public void addNodeAtEnd(Node newNode) {
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+        size++;
+    }
 
-	public void addNodeInMiddle(Node newNode) {
+    public void addNodeInMiddle(Node newNode) {
 
-		if (head == null) {
-			head = newNode;
-			tail = newNode;
-		} else {
-			INode temp, current;
-			int count = (size % 2 == 0) ? (size / 2) : ((size + 1) / 2);
-			temp = head;
-			current = null;
-			for (int i = 0; i < count; i++) {
-				current = temp;
-				temp = temp.getNext();
-			}
-			current.setNext(newNode);
-			newNode.setNext(temp);
-		}
-		size++;
-	}
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            INode temp, current = null;
+            int count = (size % 2 == 0) ? (size/2) : ((size + 1) / 2);
+            temp = head;
+            //current = null;
+            for(int i=0; i<count; i++) {
+                current = temp;
+                temp = temp.getNext();
+            }
+            current.setNext(newNode);
+            newNode.setNext(temp);
+        }
+        size++;
+    }
 
-	public void deleteNodeFromBeginning() {
-		if (head == null)
-			System.out.println("Linked List is empty!");
-		else
-			head = head.getNext();
-	}
+    public void deleteNodeFromBeginning() {
+        if(head == null)
+            System.out.println("Linked List is empty!");
+        else
+            head = head.getNext();
 
-	public void displayNode() {
-		INode current = head;
-		if (head == null)
-			System.out.println("Linked List is empty");
-		System.out.print("Linked list after deleting first node: ");
-		while (current != null) {
-			if (current.getNext() != null) {
-				System.out.print(current.getKey() + " -> ");
-				current = current.getNext();
-			} else {
-				System.out.println(current.getKey());
-				current = current.getNext();
-			}
-		}
-	}
+        size--;
+    }
 
+    public void deleteNodeFromLast() {
+        if(head == null)
+            System.out.println("Linked List is empty!");
+        else {
+            INode current = head;
+            while(tail.getNext() != null)
+                current = current.getNext();
+            current.setNext(null);
+        }
+        size--;
+    }
+
+    public void searchElement() {
+        if(head == null)
+            System.out.println("Linked List is empty");
+        else{
+            INode currentNode = head;
+            int position = 0;
+            while (currentNode != null){
+                position++;
+                if(currentNode.getKey().equals(30)){
+                    System.out.println("Key value 30 is present at position "+position+" in list");
+                }
+                currentNode = currentNode.getNext();
+            }
+        }
+    }
+
+    public void displayNode() {
+        INode current = head;
+        if(head == null)
+            System.out.println("Linked List is empty");
+        System.out.print("Nodes are: " );
+        while (current != null) {
+            if(current.getNext() != null) {
+                System.out.print(current.getKey() + " -> ");
+                current = current.getNext();
+            }
+            else {
+                System.out.println(current.getKey());
+                current = current.getNext();
+            }
+        }
+    }
 }
